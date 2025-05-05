66a84bd81bfa1e30542742589ead257711154a8e
Juan David Sevilla <sevilla.juan@correounivalle.edu.co>
2025-05-04 22:12:36 -0500

    feat(): init commit

---
+++ src/test/java/com/example/visitsservice/VisitsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.visitsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisitsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
